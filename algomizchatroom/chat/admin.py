204618cc10a467228a93e4e5cc215455f281b1df
ANUSREE _NELLIKKUNNUMMAL <anusreenarayan1@gmail.com>
2023-05-06 07:32:01 +0530

    ecom website
    ecom website with api for products

--- algomizchatroom/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from chat.models import Room, Message, Topic, User
-# Register your models here.
-
-admin.site.register(Topic)
-admin.site.register(Room)
-admin.site.register(Message)
-admin.site.register(User)
